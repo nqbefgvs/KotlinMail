f371943fb8044672b21e9ae3ee3108ac4c8e468f
nqbefgvs <389179622@qq.com>
2021-03-08 23:51:30 +0800

    3-8 simple user register ui

---
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.usercenter.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
