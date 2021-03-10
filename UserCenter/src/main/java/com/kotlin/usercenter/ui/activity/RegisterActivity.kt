45af99a4cba77dee648003af9030a1b4bcae312d
nqbefgvs <389179622@qq.com>
2021-03-10 23:58:01 +0800

    3-10 id of button(has error due to import gradle project not installed)

--- UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -2,11 +2,16 @@ package com.kotlin.usercenter.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.kotlin.usercenter.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btn_register.setOnClickListener{
+            Toast.makeText(this, "注册", Toast.LENGTH_SHORT).show();
+        }
     }
 }
